16d9e8f6d701e6abf4a6ef2b7167f0081e084189
javanshir <mcavansir10@gmail.com>
2025-12-11 20:05:04 +0400

    chore: rename Delivery.PNG to delivery.png

--- src/Features/Sections/ContactSections/DeliveryChallange.tsx
+++ src/Features/Sections/ContactSections/DeliveryChallange.tsx
@@ -1,4 +1,4 @@
-import DeliveryPhoto from '../../../assets/delivery.png';
+import DeliveryPhoto from '../../../assets/Delivery.PNG';
 
 const DeliveryBanner = () => {
 
---
+++ src/Types/assets.d.ts
@@ -0,0 +1,17 @@
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg';
+declare module '*.webp';
+declare module '*.gif';
+declare module '*.bmp';
+
+declare module '*.PNG';
+declare module '*.JPG';
+declare module '*.JPEG';
+declare module '*.SVG';
+declare module '*.WEBP';
+declare module '*.GIF';
+declare module '*.BMP';
+
+export { };
